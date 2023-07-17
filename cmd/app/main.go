bbd5b311cbd5e9eb2ec4f8041c7ef57e1f3b6531
Slava <Slava.lokshin@mail.ru>
2023-07-17 20:23:18 +0300

    Немного планов на бенчмарки

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/VyacheslavIsWorkingNow/postgis-vs-elasticsearch-performance/internal/app"
 	"github.com/VyacheslavIsWorkingNow/postgis-vs-elasticsearch-performance/internal/storage/elasticsearch"
+	"github.com/VyacheslavIsWorkingNow/postgis-vs-elasticsearch-performance/internal/storage/postgis"
 	"log"
 )
 
@@ -13,10 +14,14 @@ func main() {
 	log.Printf("app starts launching\n")
 
 	es, err := elasticsearch.New()
-
 	if err != nil {
 		log.Fatalf("can't connect to elasticsearch %e\n", err)
 	}
 
-	app.Run(es)
+	pg, err := postgis.New()
+	if err != nil {
+		log.Fatalf("can't connect to elasticsearch %e\n", err)
+	}
+
+	app.Run(pg, es)
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -2,25 +2,29 @@ package app
 
 import (
 	"context"
-	"fmt"
 	"github.com/VyacheslavIsWorkingNow/postgis-vs-elasticsearch-performance/internal/storage/elasticsearch"
+	"github.com/VyacheslavIsWorkingNow/postgis-vs-elasticsearch-performance/internal/storage/postgis"
 	"log"
 )
 
-func Run(es *elasticsearch.Storage) {
+func Run(pg *postgis.Storage, es *elasticsearch.Storage) {
 
 	log.Printf("app start\n")
-
 	ctx := context.Background()
 
-	err := es.Init(ctx)
-	if err != nil {
+	if err := es.Init(ctx); err != nil {
 		log.Fatalf("can't init es %e\n", err)
 	}
 
-	log.Printf("connect to es\n")
+	if err := pg.Init(ctx); err != nil {
+		log.Fatalf("can't init pg %e\n", err)
+	}
 
-	fmt.Println("end")
+	log.Printf("start benchmark\n")
+
+	// Запуск различных бенчев
+
+	log.Printf("end\n")
 
 }
 
